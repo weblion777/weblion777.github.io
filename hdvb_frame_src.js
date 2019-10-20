ddaaeda760352bf4b4db0c4446b186d55b50642b
weblion777 <55237297+weblion777@users.noreply.github.com>
2019-10-20 16:41:32 +0300

    Create hdvb_frame_src.js

---
+++ hdvb_frame_src.js
@@ -0,0 +1,55 @@
+!function() {
+
+    var actual = 'https://vid' + Date.now() + '.tehranvd.biz',
+        re = '^https?:\\/\\/(vii?dd?\\d*.)?{DOMAIN}',
+        https = ['makrohd.(info|com|net)', 'hdvb.(cc|xyz)', 'videolishd.(net|com)',  'farsihd.(info)', 'tehranvd.(ru|su|net|com|org|pw|biz)', 'my\-serials.(info)'],
+        list = [], delay = 200;
+
+        function listForeach (callback) {
+            https.forEach(function (value, key) {
+                if (typeof callback == 'function') {
+                callback ( value, key );
+                }
+            });
+        }
+
+        function findFrame(callback) {
+            if (document.body) 
+              return Array.prototype.find.call(document.querySelectorAll('[frame_src]'), callback);
+        }
+
+        function findFrame2(callback) {
+            if (document.body)
+                return Array.prototype.find.call(document.body.getElementsByTagName('iframe'), callback);
+        }
+
+
+        listForeach(function($value) {
+          list.push(re.replace('{DOMAIN}', $value));
+        });
+
+        findFrame(function($value) {
+          Array.prototype.find.call(list, function($reg, $reg_key) {
+          var cReg = new RegExp($reg);
+          var l = $value.attributes['frame_src'].nodeValue.match(cReg);
+          if(l !== null) if(l.length > 0) $value.setAttribute('frame_src', $value.attributes['frame_src'].nodeValue.replace(l[0], actual));
+        });
+
+
+        findFrame2(function($value) {
+            Array.prototype.find.call(list, function($reg, $reg_key) {
+                var cReg = new RegExp($reg);
+                var l = $value.src.match(cReg);
+                if(l !== null) {
+                    if(l.length > 0) {
+                        var s = $value.src.replace(l[0], actual);
+                        $value.setAttribute('src', '');
+                        setTimeout(function(){
+                            $value.setAttribute('src', s);
+                        }, 100);
+                    }
+                }
+            });
+        });
+    });
+}()
